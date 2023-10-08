2137d4c3260d53acfbf21004469474f0a1dae02e
Vivek Kumar <starktestic@gmail.com>
2023-10-08 23:39:54 +0530

    hero

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -10,7 +10,7 @@ export const LandingHero = () => {
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best way to learn is to</h1>
-        <div className="text-transparent bg-clip-text bg-white">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
               strings: ['Explore.', 'Connect.', 'Engage.', 'Discover.'],
@@ -20,7 +20,7 @@ export const LandingHero = () => {
           />
         </div>
       </div>
-      <div className="text-sm md:text-xl font-light text-zinc-200">
+      <div className="text-sm md:text-xl font-light text-zinc-400">
         Join with your kind.
       </div>
       <div>
